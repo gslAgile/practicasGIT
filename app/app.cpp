c92daa9ecf94d0d20a25f0de04d2055bf9faa56e
GabiSosaLudu <gsosaludu@gmail.com>
2021-03-14 22:08:35 -0300

    Continuo revert en master desde feature branch feature_soporte

--- app/app.cpp
+++ app/app.cpp
@@ -4,21 +4,10 @@ using namespace std;
 int main() {
   cout << "Hola, mundo!" << endl;
 
-  funcionDeSoporte(0);
   funcion_1();
   return 0;
 }
 
 void funcion_1() {
   cout << "Hola, Diego!" << endl;
-}
-
-void funcionDeSoporte( int numero) {
-    if( numero > 0) {
-        cout << "Funcion de soporte: mayor a cero" << endl;
-    } else if( numero == 0) {
-        cout << "Funcion de soporte: igual a cero" << endl;
-    }else {
-        cout << "Funcion de soporte: menor a cero" << endl;
-    }
 }
